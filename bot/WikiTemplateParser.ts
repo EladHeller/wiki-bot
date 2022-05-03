a20b75db6f4489fc8bbff01b505e1d82fa2b93e7
Elad Heller <elad@joyned.co>
2022-05-03 15:30:59 +0300

    bot: refactor to node bot.

---
+++ bot/WikiTemplateParser.ts
@@ -0,0 +1,82 @@
+/* eslint-disable no-restricted-syntax */
+
+import { nextWikiText } from './WikiParser';
+
+/* eslint-disable guard-for-in */
+export default class WikiTemplateParser {
+  articleContent: string;
+
+  templateName: string;
+
+  templateText: string;
+
+  templateData: Record<string, any>;
+
+  constructor(articleContent: string, templateName: string) {
+    this.articleContent = articleContent;
+    this.templateName = templateName;
+    this.templateText = this.findTamplateText(templateName, articleContent);
+    this.updateDataFromTemplete(this.templateText);
+  }
+
+  updateTamplateFromData(
+    templateData = this.templateData,
+    templateName = this.templateName,
+  ) {
+    let tamplateStr = `{{${templateName}\n`;
+    let value;
+    for (const key in templateData) {
+      value = templateData[key];
+      tamplateStr += `|${key}=${value}\n`;
+    }
+    tamplateStr += '}}';
+    this.templateText = tamplateStr;
+
+    return tamplateStr;
+  }
+
+  updateDataFromTemplete(templateText = this.templateText) {
+    const obj = {};
+    if (templateText) {
+      let currIndex = templateText.indexOf('|') + 1;
+
+      let pipeSignIndex;
+      let key;
+      let value;
+      let equalSignIndex;
+      let isTemplateEnd = false;
+      while (!isTemplateEnd) {
+        equalSignIndex = templateText.indexOf('=', currIndex);
+        key = templateText.substring(currIndex, equalSignIndex).trim();
+        pipeSignIndex = nextWikiText(templateText, currIndex, '|');
+        isTemplateEnd = pipeSignIndex === -1;
+
+        if (isTemplateEnd) {
+          value = templateText.substring(equalSignIndex + 1, templateText.length - 2).trim();
+        } else {
+          value = templateText.substring(equalSignIndex + 1, pipeSignIndex).trim();
+        }
+
+        obj[key] = value;
+        currIndex = pipeSignIndex + 1;
+      }
+    }
+    this.templateData = obj;
+
+    return obj;
+  }
+
+  findTamplateText(
+    templateName = this.templateName,
+    articleContent = this.articleContent,
+  ) {
+    const startStr = `{{${templateName}`;
+    let templateText = '';
+    const startIndex = articleContent.indexOf(startStr);
+    if (startIndex > -1) {
+      const endIndex = nextWikiText(articleContent, startIndex + startStr.length, '}}') + 2;
+      templateText = articleContent.substring(startIndex, endIndex);
+    }
+    return templateText;
+  }
+}
---
+++ bot/mayaAPI.ts
@@ -0,0 +1,86 @@
+import axios from 'axios';
+import { WikiPage } from './wikiAPI';
+
+const mayaLinkRegex = /^http:\/\/maya\.tase\.co\.il\/company\/(\d*)\?view=reports$/;
+const jsonLink = 'https://mayaapi.tase.co.il/api/company/financereports?companyId=';
+const companyPageLink = 'http://maya.tase.co.il/company/';
+const companyReportView = '?view=reports';
+const mayaGetOptions = {
+  method: 'get',
+  credentials: 'include',
+  headers: {
+    'X-Maya-With': 'allow',
+    origin: 'https://maya.tase.co.il',
+    referer: 'https://maya.tase.co.il/',
+    accept: 'application/json',
+    authority: 'mayaapi.tase.co.il',
+  },
+};
+
+type Periond = {
+  Title: string;
+  Year: number;
+  IFRS: boolean;
+  IsPreview: boolean;
+}
+
+type CompanyData = {
+  Title: string | null,
+  Rows: [];
+}
+type ComponyRow = {
+  SectionId: number,
+  Code: string;
+  Name: string;
+  CurrPeriodValue: string;
+  PrevPeriodValue: string;
+  PrevYearValue: string;
+}
+export type MayaCompany = {
+  CurrentPeriod: Periond;
+  PreviousPeriod: Periond;
+  PreviousYear: Periond;
+  CurrencyCode: number;
+  CurrencyName: string;
+  Factor: number;
+  CompanyShortName: string;
+  IsPeriod: boolean;
+  Balance: CompanyData;
+  ProfitReport: CompanyData;
+  AdditionalData: CompanyData;
+  FinancialRatios: CompanyData;
+  AllRows: ComponyRow[],
+  LastReports: {
+      RptCd: number,
+      Title: string;
+      PubDate: string;
+      CompanyId: number;
+      StatusDesc: string;
+  }[]
+};
+
+export type MayaWithWiki = {
+  maya: MayaCompany;
+  wiki: WikiPage;
+}
+
+export default async function getMayaDetails(
+  wikiPage: WikiPage,
+): Promise<MayaWithWiki | undefined> {
+  const extLink = wikiPage.extlinks?.find((link) => link['*'].match(mayaLinkRegex))?.['*'];
+  if (!extLink) {
+    console.error('No extlinks', wikiPage.title, wikiPage.extlinks);
+    return undefined;
+  }
+  const companyFinnaceDetailsUrl = extLink.replace(companyPageLink, jsonLink).replace(companyReportView, '');
+
+  return axios(companyFinnaceDetailsUrl, mayaGetOptions)
+    .then((result) => ({
+      maya: result.data,
+      wiki: wikiPage,
+    }))
+    .catch((e) => {
+      console.error(e);
+      return undefined;
+    });
+}
