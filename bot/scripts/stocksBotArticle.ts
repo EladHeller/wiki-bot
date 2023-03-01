6f26b16c97f4e9c93a2374709a5418db88dae1ac
Elad Heller <elad.heller@applitools.com>
2023-03-01 22:11:01 +0200

    stock bot articles script.

---
+++ bot/scripts/stocksBotArticle.ts
@@ -0,0 +1,53 @@
+import 'dotenv/config';
+import { getMarketValue } from '../mayaAPI';
+import { promiseSequence } from '../utilities';
+import {
+  getToken, login, getMayaLinks, WikiPage, updateArticle,
+} from '../wikiAPI';
+import WikiTemplateParser from '../WikiTemplateParser';
+
+async function main() {
+  const logintoken = await getToken();
+  await login(logintoken);
+  console.log('Login success');
+
+  const results = await getMayaLinks(true);
+  const marketValues:{
+    page: WikiPage,
+    id: number,
+  }[] = [];
+  for (const page of Object.values(results)) {
+    const res = await getMarketValue(page);
+    if (res?.marketValue) {
+      console.log(page.title);
+      marketValues.push({
+        page,
+        id: res.id,
+      });
+    }
+  }
+
+  console.log('finnish get links', marketValues.length);
+
+  await promiseSequence(10, marketValues.map(({ page, id }) => async () => {
+    try {
+      const content = page.revisions[0].slots.main['*'];
+      console.log(page.title);
+      const template = new WikiTemplateParser(content, 'חברה מסחרית');
+      const { templateText } = template;
+      const marketCap = template.templateData['שווי'];
+      const marketCapDate = template.templateData['תאריך שווי שוק'];
+      if (!marketCap || !marketCapDate || !marketCap?.includes('שווי שוק חברה בורסאית')) {
+        template.templateData['שווי'] = `{{שווי שוק חברה בורסאית|ID=${id}}}`;
+        template.templateData['תאריך שווי שוק'] = '{{שווי שוק חברה בורסאית|ID=timestamp}}';
+        const newContent = content.replace(templateText, template.updateTamplateFromData() + (templateText ? '' : '\n'));
+        console.log(await updateArticle(page.title, 'הוספת תבנית שווי שוק חברה בורסאית', newContent));
+        console.log(page.title, 'updated');
+      }
+    } catch (e) {
+      console.error(page.title, e);
+    }
+  }));
+}
+
+main().catch(console.error);
