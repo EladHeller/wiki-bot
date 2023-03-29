575dc6a322f534d8518949dd8e19f24df8eb344e
Elad Heller <elad.heller@applitools.com>
2023-03-29 22:25:15 +0300

    replace bot

--- bot/scripts/replace.ts
+++ bot/scripts/replace.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import express from 'express';
 import {
   login, updateArticle, search,
 } from '../wikiAPI';
@@ -8,14 +9,14 @@ const link = '%d7%';
 async function main() {
   await login();
 
-  const res = search(link);
+  const res = search(link, 5, 5);
   let curr = await res.next();
 
   while (curr.done === false) {
     curr.value?.forEach(async (page) => {
       const content = page.revisions?.[0].slots.main['*'];
       if (content && page.title) {
-        const matches = content.match(/(?:%d7%[9aA][0-9a-fA-F]){3,}/gi);
+        const matches = content.match(/(?:%d7%[9aA][0-9a-fA-F]){2,}/gi);
         let newContent = content;
         if (!matches) {
           return;
@@ -27,7 +28,7 @@ async function main() {
           return;
         }
         try {
-          await updateArticle(page.title, 'קידוד קישור', newContent);
+          console.log(page.title, 'קידוד קישור', newContent);
         } catch (error) {
           console.log(error?.data || error?.message || error?.toString());
         }
@@ -37,5 +38,13 @@ async function main() {
     curr = await res.next();
   }
 }
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
 
 main().catch(console.error);
