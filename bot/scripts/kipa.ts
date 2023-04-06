9e6b5c2b560f0ef2fa12ca783b6200375b3d2d33
Elad Heller <elad.heller@applitools.com>
2023-04-07 02:13:43 +0300

    kipa: refactor.

--- bot/scripts/kipa.ts
+++ bot/scripts/kipa.ts
@@ -7,6 +7,29 @@ import { findTemplates } from '../newTemplateParser';
 
 const TEMPLATE_NAME = 'כיפה';
 const url = 'https://www.kipa.co.il/';
+
+async function fixParameters(content: string, title: string): Promise<string> {
+  let newContent = content;
+  const templates = findTemplates(content, TEMPLATE_NAME, title);
+  if (!templates.length) {
+    console.log('no template', title);
+  }
+  await Promise.all(templates.map(async (template) => {
+    const parameters = template.replace(`{{${TEMPLATE_NAME}|`, '').replace('}}', '').split('|');
+    if (parameters[2].match(/^[0-9a-zA-Z]+(?:\/[0-9a-zA-Z]*)+$/)) {
+      const res = await fetch(`${url}${parameters[2]}${!parameters[2].startsWith('ask') ? '.html' : ''}`);
+      const newUrl = decodeURIComponent(res.url.replace(url, ''));
+      if (newUrl) {
+        parameters[2] = decodeURIComponent(res.url.replace(url, ''));
+        newContent = newContent.replace(template, `{{${TEMPLATE_NAME}|${parameters.join('|')}}}`);
+      } else {
+        console.log('no new url', title);
+      }
+    }
+  }));
+  return newContent;
+}
+
 async function main() {
   await login();
   const results = await getArticleWithKipaTemplate();
@@ -19,25 +42,7 @@ async function main() {
         console.log('no content', result.title);
         return;
       }
-      let newContent = content;
-      const templates = findTemplates(content, TEMPLATE_NAME, result.title);
-      if (!templates.length) {
-        console.log('no template', result.title);
-        return;
-      }
-      await Promise.all(templates.map(async (template) => {
-        const parameters = template.replace(`{{${TEMPLATE_NAME}|`, '').replace('}}', '').split('|');
-        if (parameters[2].match(/^[0-9a-zA-Z]+(?:\/[0-9a-zA-Z]*)+$/)) {
-          const res = await fetch(`${url}${parameters[2]}${!parameters[2].startsWith('ask') ? '.html' : ''}`);
-          const newUrl = decodeURIComponent(res.url.replace(url, ''));
-          if (newUrl) {
-            parameters[2] = decodeURIComponent(res.url.replace(url, ''));
-            newContent = newContent.replace(template, `{{${TEMPLATE_NAME}|${parameters.join('|')}}}`);
-          } else {
-            console.log('no new url', result.title);
-          }
-        }
-      }));
+      const newContent = await fixParameters(content, result.title);
       if (newContent === content) {
         return;
       }
