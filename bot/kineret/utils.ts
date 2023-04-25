a65c9bf34ec49d15f8a89557deed6938d95742ae
Elad Heller <elad.heller@applitools.com>
2023-04-25 13:29:58 +0300

    kineret: update change field.

--- bot/kineret/utils.ts
+++ bot/kineret/utils.ts
@@ -18,7 +18,27 @@ export function formatDate(date: Date) {
 
 const DATE_LEVEL_FIELD = 'תאריך גובה';
 const LEVEL_FIELD = 'גובה';
+const CAHNGE_FIELD = 'שינוי';
 const TEMPLATE_NAME = 'גוף מים';
+
+function getChangeData(change: number) {
+  if (change === 0) {
+    return {
+      text: 'ללא שינוי',
+      icon: '[[File:Steady2.svg|11px]]',
+    };
+  }
+  if (change > 0) {
+    return {
+      text: 'עלייה',
+      icon: '[[File:Increase2.svg|11px]]',
+    };
+  }
+  return {
+    text: 'ירידה',
+    icon: '[[File:Decrease2.svg|11px]]',
+  };
+}
 
 export async function updateLevel(
   levelData: LevelData,
@@ -44,10 +64,15 @@ export async function updateLevel(
     return;
   }
 
+  const change = Number(level) - Number(template.templateData[LEVEL_FIELD]);
+  const changeData = getChangeData(change);
+  const oldDate = template.templateData[DATE_LEVEL_FIELD];
+
   const newTemplateText = template.updateTamplateFromData({
     ...template.templateData,
     [dateLevelField]: date,
     [levelField]: level,
+    [CAHNGE_FIELD]: `${changeData.text} של ${Math.abs(change * 100)} ס"מ ${changeData.icon} מ-${oldDate}`,
   });
   const newContent = content.replace(oldTemplate, newTemplateText);
 
