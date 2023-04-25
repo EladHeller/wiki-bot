fad4781f9f2ecc9a4e8dbfe8dd813ed8fad56eb9
Elad Heller <elad.heller@applitools.com>
2023-04-25 23:55:46 +0300

    tag users.

--- bot/admin/deleteRedirects.ts
+++ bot/admin/deleteRedirects.ts
@@ -5,10 +5,12 @@ import {
 } from '../wikiAPI';
 import { WikiPage } from '../types';
 import { promiseSequence } from '../utilities';
+import writeAdminBotLogs, { ArticleLog } from './log';
 
 async function deleteRedirects(from: number, to: number[], title: string, reason: string) {
   const generator = getRedirects(from, to);
   const all: WikiPage[] = [];
+  const errors: string[] = [];
   let res;
   try {
     do {
@@ -17,39 +19,75 @@ async function deleteRedirects(from: number, to: number[], title: string, reason
       const relevent = batch.filter((x) => x.links?.length === 1);
       all.push(...relevent);
       await promiseSequence(10, relevent.map((p: WikiPage) => async () => {
-        await deletePage(p.title, reason);
+        try {
+          await deletePage(p.title, reason);
+        } catch (error) {
+          errors.push(p.title);
+          console.log(error?.data || error?.message || error?.toString());
+        }
       }));
     } while (!res.done);
   } catch (error) {
     console.log(error?.data || error?.message || error?.toString());
   }
   const unique = all.filter((v, i, a) => a.findIndex((t) => t.title === v.title) === i);
-  console.log(unique.map((x) => `[[${x.title}]] ${x.links?.length === 1 ? ` {{כ}}← [[${x.links?.[0].title}]]` : 'לא ברור'}`).join('\n'));
+  const logs: ArticleLog[] = unique.map((x) => {
+    const error = errors.includes(x.title);
+    const skipped = x.links?.length !== 1;
+    return {
+      text: `[[${x.title}]] ${x.links?.length === 1 ? ` {{כ}}← [[${x.links?.[0].title}]]` : 'לא ברור'}${error ? ' - שגיאה' : ''}`,
+      error,
+      skipped,
+    };
+  });
+  return logs;
 }
 
 async function deleteInCategory(category: string, reason: string, match?: RegExp) {
   const generator = listCategory(category);
   let res;
+  const logs: ArticleLog[] = [];
   try {
     do {
       res = await generator.next();
       const batch: WikiPage[] = res.value?.query.categorymembers ?? [];
       await promiseSequence(10, batch.map((p: WikiPage) => async () => {
-        if (match && !p.title.match(match)) return;
-        await deletePage(p.title, reason);
+        if (match && !p.title.match(match)) {
+          logs.push({
+            text: `[[${p.title}]]`,
+            skipped: true,
+          });
+          return;
+        }
+        try {
+          await deletePage(p.title, reason);
+          logs.push({
+            text: `[[${p.title}]]`,
+          });
+        } catch (error) {
+          logs.push({
+            text: `[[${p.title}]]`,
+            error: true,
+          });
+          console.log(error?.data || error?.message || error?.toString());
+        }
       }));
     } while (!res.done);
   } catch (error) {
     console.log(error?.data || error?.message || error?.toString());
   }
+  return logs;
 }
 
 export async function main() {
   await login();
   console.log('logged in');
-  await deleteInCategory('ויקיפדיה/בוט/בוט ההסבה/דפי פלט/למחיקה', 'דף פלט של בוט ההסבה', /\/דוגמאות|\/פלט/);
-  await deleteRedirects(119, [1], 'user:Sapper-bot/הפניות שיחה טיוטה לשיחה', 'הפניה ממרחב שיחת טיוטה למרחב השיחה');
-  await deleteRedirects(118, [0], 'user:Sapper-bot/הפניות טיוטה לראשי', 'הפניה ממרחב הטיוטה למרחב הערכים');
-  await deleteRedirects(3, [1], 'user:Sapper-bot/הפניות שיחת משתמש לשיחה', 'הפניה ממרחב שיחת משתמש למרחב שיחה');
-  await deleteRedirects(0, [2, 118], 'user:Sapper-bot/הפניות ראשי למשתמש או טיוטה', 'הפניה ממרחב ראשי למרחב טיוטה');
+  const convertLogs = await deleteInCategory('ויקיפדיה/בוט/בוט ההסבה/דפי פלט/למחיקה', 'דף פלט של בוט ההסבה', /\/דוגמאות|\/פלט/);
+  const logs: ArticleLog[] = [];
+  logs.push(...(await deleteRedirects(119, [1], 'user:Sapper-bot/הפניות שיחה טיוטה לשיחה', 'הפניה ממרחב שיחת טיוטה למרחב השיחה')));
+  logs.push(...(await deleteRedirects(118, [0], 'user:Sapper-bot/הפניות טיוטה לראשי', 'הפניה ממרחב הטיוטה למרחב הערכים')));
+  logs.push(...(await deleteRedirects(3, [1], 'user:Sapper-bot/הפניות שיחת משתמש לשיחה', 'הפניה ממרחב שיחת משתמש למרחב שיחה')));
+  logs.push(...(await deleteRedirects(0, [2, 118], 'user:Sapper-bot/הפניות ראשי למשתמש או טיוטה', 'הפניה ממרחב ראשי למרחב טיוטה')));
+  await writeAdminBotLogs(logs, 'משתמש:Sapper-bot/מחיקת הפניות חוצות מרחבי שם');
+  await writeAdminBotLogs(convertLogs, 'משתמש:Sapper-bot/מחיקת דפי פלט של בוט ההסבה');
 }
---
+++ bot/admin/log.ts
@@ -0,0 +1,55 @@
+import { getLocalDate } from '../utilities';
+import { getArticleContent, updateArticle } from '../wikiAPI';
+
+export interface ArticleLog {
+    text: string;
+    error?: boolean;
+    skipped?: boolean;
+}
+
+const tagsPage = 'משתמש:Sapper-bot/תיוג משתמשים';
+
+function getContentFromLogs(logs: ArticleLog[]): string {
+  if (!logs.length) {
+    return '';
+  }
+  return `* ${logs.map(({ text }) => text).join('\n* ')}\n`;
+}
+
+let tagsPageContent: string| undefined;
+
+async function getUsersToTag(users: string[] = []): Promise<string> {
+  if (tagsPageContent == null) {
+    tagsPageContent = await getArticleContent(tagsPage);
+  }
+  const dynamicTagUsers = tagsPageContent?.split('\n').map((line) => line.replace('*', '').trim()) ?? [];
+  if (!dynamicTagUsers.length && !users.length) {
+    return '';
+  }
+  return `${[...new Set([...users, ...dynamicTagUsers])].join(', ')} לידיעתכם. ~~~~`;
+}
+
+export default async function writeAdminBotLogs(
+  logs: ArticleLog[],
+  logPageTitle: string,
+  users?: string[],
+) {
+  if (!logs.length) {
+    return;
+  }
+
+  const success = logs.filter((log) => !log.error && !log.skipped);
+  const errors = logs.filter((log) => log.error);
+  const skipped = logs.filter((log) => log.skipped);
+  const successContent = getContentFromLogs(success);
+  const errorContent = `${errors.length ? '===שגיאות===\n' : ''}${getContentFromLogs(errors)}`;
+  const skippedContent = `${skipped.length ? '===דפים שדולגו===\n' : ''}${getContentFromLogs(skipped)}`;
+  const usersToTag = await getUsersToTag(users);
+  const titleAndSummary = `לוג ריצה ${getLocalDate(new Date().toLocaleString())}`;
+  await updateArticle(
+    logPageTitle,
+    titleAndSummary,
+    `${successContent}${errorContent}${skippedContent}\n${usersToTag}`,
+    titleAndSummary,
+  );
+}
