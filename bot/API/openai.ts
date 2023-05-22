741be363062c0fcb97e960830c528c78348e314b
Elad Heller <elad.heller@applitools.com>
2023-05-22 11:37:58 +0300

    openai: q&a example.

--- bot/API/openai.ts
+++ bot/API/openai.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Configuration, OpenAIApi } from 'openai';
+import { ChatCompletionRequestMessageRoleEnum, Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -26,6 +26,55 @@ export async function isTwoWordsIsTheSamePerson(
   return isEqual;
 }
 
+type QuestionAndAnswer = {
+  question: string;
+  answer: string;
+};
+
+const previewAnswers: QuestionAndAnswer[] = [];
+
+/**
+  console.log(await chatWithTerminal('cd ~'));
+  console.log(await chatWithTerminal('mkdir Docs'));
+  console.log(await chatWithTerminal('cd ./Docs'));
+  console.log(await chatWithTerminal('echo "Hello World" > hello.txt'));
+  console.log(await chatWithTerminal('cat hello.txt'));
+  console.log(await chatWithTerminal('pwd'));
+  console.log(await chatWithTerminal('ls'));
+  console.log(await chatWithTerminal('cd ..'));
+  console.log(await chatWithTerminal('ls'));
+ */
+export async function chatWithTerminal(prompt: string): Promise<string> {
+  const res = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo-0301',
+    messages: [
+      {
+        role: 'system',
+        content: `Please answear only in this site context:
+You are red hat linux bash terminal please just print the base output without any explanations.
+If there is no output just print empty line.`,
+      },
+      ...previewAnswers.flatMap((qa) => [
+        {
+          role: 'user' as ChatCompletionRequestMessageRoleEnum,
+          content: qa.question,
+        },
+        {
+          role: 'assistant' as ChatCompletionRequestMessageRoleEnum,
+          content: qa.answer,
+        },
+      ]),
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    temperature: 0.2,
+  });
+  const answer = res.data.choices[0].message?.content ?? '';
+  previewAnswers.push({ question: prompt, answer });
+  return answer;
+}
 export default {
   isTwoWordsIsTheSamePerson,
 };
--- bot/experiments/index.ts
+++ bot/experiments/index.ts
@@ -1,25 +1,8 @@
 import 'dotenv/config';
-import { isTwoWordsIsTheSamePerson } from '../API/openai';
+import { chatWithTerminal } from '../API/openai';
 
 async function main() {
-  const questions = [
-    ['[[עפרה שטראוס]]', 'ד״ר עופרה שטראוס'],
-    ['[[מיכה מייקסנר]]', 'מיכאל מייקסנר'],
-    ['[[דינה בן טל גננסיה]]', 'דינה גננסיה בן טל'],
-    ['[[מאיר שפיגלר]]', 'שפיגלר בן אורי מאיר'],
-    ['איתי בן זאב', 'בן זאב איתי'],
-    ['שלומי טחן', 'שלמה טחן'],
-    ['משה אביגדור כהן', 'משה אביגדור לוי'],
-    ['[[חנן פרידמן]]', 'פרידמן חנן שמואל'],
-    ['[[סמדר ברבר-צדיק]]', 'סמדר ברבר-צדיק'],
-    ['[[הראל ויזל]]', 'ויזל הראל אליעזר'],
-    ['[[עידן וולס]]', 'עידן ולס'],
-  ];
-  for (const q of questions) {
-    const [nameFromWiki, name2] = q;
-    const curr = await isTwoWordsIsTheSamePerson(nameFromWiki, name2);
-    console.log(q, curr);
-  }
+  console.log(await chatWithTerminal('curl http://example.com'));
 }
 
 main();
