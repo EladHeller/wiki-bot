bc2207ceb97087cf4da972f42bf537a0804f1462
Elad Heller <elad.heller@applitools.com>
2023-06-04 20:43:30 +0300

    jewish books fixes.

--- bot/scripts/jewishBooksTemplate.ts
+++ bot/scripts/jewishBooksTemplate.ts
@@ -1,4 +1,3 @@
-import fs from 'fs/promises';
 import { asyncGeneratorMapWithSequence } from '../utilities';
 import { getArticlesWithTemplate, updateArticle } from '../wiki/wikiAPI';
 
@@ -6,10 +5,9 @@ const TEMPLATE_NAME = 'אוצר הספרים היהודי';
 
 export default async function jewishBooks() {
   const generator = getArticlesWithTemplate(`תבנית:${TEMPLATE_NAME}`);
-  let res = '';
 
   await asyncGeneratorMapWithSequence(10, generator, (page) => async () => {
-    const content = page.revisions[0].slots.main['*'];
+    const content = page.revisions?.[0].slots.main['*'];
     if (!content) {
       console.log('no content', page.title);
       return;
@@ -20,12 +18,10 @@ export default async function jewishBooks() {
     }
     const newContent = content.replace(/\n{{אוצר הספרים היהודי/g, `\n* {{${TEMPLATE_NAME}`);
     if (newContent !== content) {
-      res += `${page.title}\n${newContent}\n\n`;
       await updateArticle(page.title, 'עיצוב תבנית', newContent);
       console.log(page.ns, page.title);
     } else {
       console.log('no change', page.title);
     }
   });
-  await fs.writeFile('jewishBooks.txt', res);
 }
