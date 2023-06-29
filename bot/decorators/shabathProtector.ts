e3c9503e6d3828e7469818566aee20a8498111b7
Elad Heller <elad.heller@applitools.com>
2023-06-29 08:46:56 +0300

    shabath and yom tov protector with hebcal.

--- bot/decorators/shabathProtector.ts
+++ bot/decorators/shabathProtector.ts
@@ -1,17 +1,44 @@
+/* eslint-disable no-bitwise */
+import {
+  HDate, HebrewCalendar, Zmanim, flags,
+} from '@hebcal/core';
+
+const JERUSALEM_LATITUDE = 31.7784;
+const JERUSALEM_LONGITUDE = 35.2354;
+
 export default function shabathProtectorDecorator(cb: (...args: any[]) => any) {
   return async function shabathProtector(...args: any[]) {
-    const currentDateUTC = new Date(); // Lambda is in UTC
-    const day = currentDateUTC.getDay();
-    const hour = currentDateUTC.getHours();
-    /**
-       * The earliar Shabath starts in Israel, is about 16:00 on Friday in winter (14 because
-       * Israel is GMT+2)
-       * The latest Shabath ends is about 20:30 on Saturday in summer (17 becayse Israel is GMT+3
-       * in summer)
-       */
-    const isShabathInIsrael = (day === 5 && hour >= 14) || (day === 6 && hour <= 17);
+    const now = new Date();
+    const zman = new Zmanim(now, JERUSALEM_LATITUDE, JERUSALEM_LONGITUDE);
+    // 60 minutes before sunset to be on the safe side
+    const isAfterStart = now > zman.sunsetOffset(-60);
+    // 60 minutes after sunset to be on the safe side
+    const isBeforeEnd = now < zman.sunsetOffset(60);
+    const day = new HDate(new Date(now));
+    const holidays = HebrewCalendar.getHolidaysOnDate(day, true);
+    const tomorowHolidays = HebrewCalendar.getHolidaysOnDate(day.add(1, 'd'), true);
+    const dayNumber = day.getDay();
+
+    const isShabathInIsrael = (dayNumber === 5 && isAfterStart)
+       || (dayNumber === 6 && isBeforeEnd);
+
+    let isYomTovInIsrael = false;
+    holidays.forEach((holiday) => {
+      if (((holiday.mask & flags.CHAG) && isBeforeEnd)) {
+        isYomTovInIsrael = true;
+      }
+    });
+    tomorowHolidays.forEach((tomorowHoliday) => {
+      if (((tomorowHoliday.mask & flags.CHAG) && isAfterStart)) {
+        isYomTovInIsrael = true;
+      }
+    });
     if (isShabathInIsrael) {
       console.log('שבת היום להשם https://www.youtube.com/watch?v=oGZrUH88pnc');
+      return undefined;
+    }
+    if (isYomTovInIsrael) {
+      console.log('גוט יום טוב https://www.youtube.com/watch?v=oGZrUH88pnc');
       return undefined;
     }
     return cb(...args);
