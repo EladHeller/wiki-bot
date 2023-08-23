ea593a032ac6d868a4aa6dcebace0fe66b773c9e
Elad Heller <elad.heller@applitools.com>
2023-08-23 15:17:23 +0300

    external link parser.

--- bot/__tests__/wikiLinkParser.ts
+++ bot/__tests__/wikiLinkParser.ts
@@ -1,4 +1,6 @@
-import { getInnerLinks, getInnerLink } from '../wiki/wikiLinkParser';
+import {
+  getInnerLinks, getInnerLink, getExteranlLinks, getExteranlLink,
+} from '../wiki/wikiLinkParser';
 
 describe('getInnerLinks', () => {
   it('should return an array of InnerLink objects', () => {
@@ -48,5 +50,60 @@ describe('getInnerLink', () => {
     const result = getInnerLink(mockText);
 
     expect(result).toStrictEqual(expectedLink);
+  });
+});
+
+describe('getExteranlLinks', () => {
+  it('should ignore inner links', () => {
+    const mockText = '[[Link1|Text1]] [https://example.com text of link] [[Link2]] [[Link3|Text3]]';
+
+    const expectedLinks = [
+      { link: 'https://example.com', text: 'text of link' },
+    ];
+
+    const result = getExteranlLinks(mockText);
+
+    expect(result).toStrictEqual(expectedLinks);
+  });
+
+  it('should return an array of links', () => {
+    const mockText = '[https://example.com text of link] [ https://other.com ] [https://third.com description]';
+
+    const expectedLinks = [
+      { link: 'https://example.com', text: 'text of link' },
+      { link: 'https://other.com', text: '' },
+      { link: 'https://third.com', text: 'description' },
+    ];
+
+    const result = getExteranlLinks(mockText);
+
+    expect(result).toStrictEqual(expectedLinks);
+  });
+
+  it('should return an empty array if there are no exteranal links', () => {
+    const mockText = 'No links';
+
+    const result = getExteranlLinks(mockText);
+
+    expect(result).toStrictEqual([]);
+  });
+});
+
+describe('getExteranlLink', () => {
+  it('should return the first link', () => {
+    const mockText = '[https://example.com text of link] [ https://other.com ] [https://third.com description]';
+    const expectedLink = { link: 'https://example.com', text: 'text of link' };
+
+    const result = getExteranlLink(mockText);
+
+    expect(result).toStrictEqual(expectedLink);
+  });
+
+  it('should return undefined if there are no exteranal links', () => {
+    const mockText = 'No links';
+
+    const result = getExteranlLink(mockText);
+
+    expect(result).toBeUndefined();
   });
 });
