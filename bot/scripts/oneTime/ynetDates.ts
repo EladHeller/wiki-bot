42af4e9f51a9554a9a6961019f0c2f1e0b21bc04
Elad Heller <elad.heller@applitools.com>
2023-09-03 15:24:19 +0300

    ynet template dates.

--- bot/scripts/oneTime/ynetDates.ts
+++ bot/scripts/oneTime/ynetDates.ts
@@ -1,17 +1,34 @@
+import { JSDOM } from 'jsdom';
 import { WikiPage } from '../../types';
 import {
-  asyncGeneratorMapWithSequence, getFullYear, getLocalDate, parseLocalDate,
+  asyncGeneratorMapWithSequence, getFullYear, getLocalDate, parseLocalDate, promiseSequence,
 } from '../../utilities';
 import NewWikiApi from '../../wiki/NewWikiApi';
 import { findTemplates, getTemplateArrayData } from '../../wiki/newTemplateParser';
+import { getAttr, getSchemaData } from '../../scraping';
 
 const TEMPLATE_NAME = 'ynet';
+
+async function getDateFromYnetPage(url: string, title: string) {
+  try {
+    const dom = await JSDOM.fromURL(url);
+    const displayDate = getAttr(dom.window.document, 'time.DateDisplay', 'datetime');
+    const datePublished = getSchemaData(dom.window.document, 'NewsArticle')?.datePublished?.toString();
+    const date = getLocalDate(datePublished)
+    || (displayDate && getLocalDate(displayDate))
+    || '';
+    return date;
+  } catch (error) {
+    console.log('Failed to get date from ynet page', url, title);
+    return '';
+  }
+}
 
 export default async function ynetDates() {
   const api = NewWikiApi();
   await api.login();
   const generator = api.categroyPages('שגיאות פרמטריות בתבנית Ynet');
-  await asyncGeneratorMapWithSequence<WikiPage>(25, generator, (page) => async () => {
+  await asyncGeneratorMapWithSequence<WikiPage>(1, generator, (page) => async () => {
     const content = page.revisions?.[0].slots.main['*'];
     if (!content) {
       console.log('Missing content', page.title);
@@ -19,10 +36,10 @@ export default async function ynetDates() {
     }
     let newContent = content;
     const ynetTemplates = findTemplates(newContent, TEMPLATE_NAME, page.title);
-    ynetTemplates.forEach((ynetTemplate) => {
+    await promiseSequence(1, ynetTemplates.map((ynetTemplate) => async () => {
       const arrayData = getTemplateArrayData(ynetTemplate, TEMPLATE_NAME, page.title, true);
-      const date = arrayData[3];
-      if (!date) {
+      const [,, id, date] = arrayData;
+      if (!date || !id) {
         return;
       }
       const justDate = date.replace('{{כ}}', '')
@@ -30,6 +47,25 @@ export default async function ynetDates() {
         .replace('פורסם ב-', '')
         .replace('פורסם: ', '')
         .replace(/באתר \[?\[?YNET\]?\]?/i, '');
+
+      if (
+        justDate.match(/^([א-ת]{3,10},? )?\d{4}[.,]?$/)
+        || justDate.match(/^\d{1,2} ב[א-ת]{3,9}[.,]?$/)
+        || justDate.match(/^ב?\[?\[?[א-ת]{3,9}\]?\]?[,.]? \[?\[?\d{4}\]?\]?[,.]?$/)) {
+        let url: string;
+        if (id.match(/^\d{2,10}$/)) {
+          url = `https://www.ynet.co.il/articles/0,7340,L-${id},00.html`;
+        } else {
+          url = `https://www.ynet.co.il/${id}`;
+        }
+        const dateFromDoc = await getDateFromYnetPage(url, page.title);
+        if (dateFromDoc) {
+          const newTemplateText = ynetTemplate.replace(date, dateFromDoc);
+          newContent = newContent.replace(ynetTemplate, newTemplateText);
+          return;
+        }
+        console.log('Failed to get date from ynet page', url, page.title);
+      }
       const withLinkMatch = justDate.match(/^[הב]?\[?\[?(\d{1,2} ?ב[א-ת]{3,9})\]?\]?,? ?\[?\[?(\d{4})\]?\]?[.,]?$/);
       if (withLinkMatch) {
         const newTemplateText = ynetTemplate.replace(date, `${withLinkMatch[1]} ${withLinkMatch[2]}`);
@@ -71,7 +107,7 @@ export default async function ynetDates() {
       }
       const newTemplateText = ynetTemplate.replace(date, localDate);
       newContent = newContent.replace(ynetTemplate, newTemplateText);
-    });
+    }));
     if (newContent !== content) {
       await api.updateArticle(page.title, 'תבנית ynet: תיקון פורמט תאריך', newContent);
       console.log('Updated', page.title);
