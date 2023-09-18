9dbb82baeb16ad3c7a26560ee15f3e8b33d0a377
Elad Heller <elad.heller@applitools.com>
2023-09-18 10:49:59 +0300

    log: prevent duplicate log.

--- bot/experiments/index.ts
+++ bot/experiments/index.ts
@@ -1,10 +1,7 @@
 import 'dotenv/config';
-import haaretzDates from '../scripts/oneTime/templatesDates/haaretzDates';
-import pagesWithoutProtectInMainPage from '../admin/pagesWithoutProtectInMainPage';
 
+// eslint-disable-next-line no-empty-function
 async function main() {
-  const res = await pagesWithoutProtectInMainPage();
-  console.log(res);
 }
 
 main();
