a2093e8859b3cf3fa9e64acbdc9f8556137e5b69
Elad Heller <eladheller@gmail.com>
2024-09-02 10:06:56 +0300

    lambda script: fix protect.

--- bot/experiments/lambda.ts
+++ bot/experiments/lambda.ts
@@ -5,6 +5,7 @@ export async function main() {
   const api = NewWikiApi();
   try {
     await api.login();
+    await api.protect('תבנית:דגל/שימאנה (מחוז)', '', 'never', 'באג של ויקיפדיה');
     await api.protect('תבנית:דגל/שימאנה (מחוז)', 'edit=editautopatrolprotected|move=editautopatrolprotected', 'never', 'תבנית דגל: בשימוש רב');
     console.log('Done');
   } catch (e) {
