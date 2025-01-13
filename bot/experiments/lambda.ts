0bfbdebc6cbe158a9c3379787c2dc63ef0e65da5
Elad Heller <eladheller@gmail.com>
2025-01-13 13:56:37 +0200

    lambda test: check playwright.

--- bot/experiments/lambda.ts
+++ bot/experiments/lambda.ts
@@ -1,14 +1,22 @@
 import 'dotenv/config';
-import { JSDOM } from 'jsdom';
+import * as playwright from 'playwright-aws-lambda';
+import { ChromiumBrowser, Page } from 'playwright-core';
 
 export async function main() {
+  let page: Page;
+  let browser: ChromiumBrowser;
+
   try {
-    await JSDOM.fromURL('https://kineret.org.il/');
-  } catch (e) {
-    console.error(e);
-  }
-  try {
-    await fetch('https://kineret.org.il/');
+    browser = await playwright.launchChromium({
+      headless: false,
+      timeout: 10 * 1000,
+    });
+    const context = await browser.newContext();
+    page = await context.newPage();
+    await page.goto('https://www.idf.il/אתרי-יחידות/יומן-המלחמה/חללי-ופצועי-צה-ל-במלחמה/');
+    const content = await page.content();
+    console.log(content);
+    await browser.close();
   } catch (e) {
     console.error(e);
   }
