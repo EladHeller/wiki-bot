3366f7f16da01fcc22b563e31a18364c6811326e
Elad Heller <eladheller@gmail.com>
2025-01-19 12:36:05 +0200

    iron swords: refactor.

---
+++ bot/ironSwords/idfSiteScraper.ts
@@ -0,0 +1,54 @@
+import * as playwright from 'playwright-aws-lambda';
+import { JSDOM } from 'jsdom';
+import { Browser } from 'playwright-core';
+
+const url = 'https://www.idf.il/אתרי-יחידות/יומן-המלחמה/חללי-ופצועי-צה-ל-במלחמה/';
+
+function getCounter(
+  counters: Element[],
+  title: string,
+  index?: number,
+): number | null {
+  const elements = counters.filter((counter) => {
+    const text = counter.textContent;
+    return text?.includes(title);
+  });
+  if (elements.length > 1 && index == null) {
+    throw new Error(`Multiple counters with title ${title}`);
+  }
+  const element = elements[index ?? 0];
+  const numberElement = element?.querySelector('.counters');
+  const text = numberElement?.textContent;
+  if (!text) {
+    throw new Error(`Counter with title ${title} not found`);
+  }
+  return text ? Number(text.trim().replaceAll(',', '')) : null;
+}
+
+export default async function getCasualties() {
+  let browser: Browser | null = null;
+  try {
+    browser = await playwright.launchChromium({
+      headless: false,
+      timeout: 10 * 1000,
+    });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto(url);
+    const content = await page.content();
+    const { window } = new JSDOM(content);
+    const counters = Array.from(window.document.querySelectorAll('.counter-parent'));
+    const soldiersKilled = getCounter(counters, 'חללים מתחילת המלחמה');
+    const soldiersKilledManeuver = getCounter(counters, 'חללים מהתמרון בעזה');
+    const soldiersWounded = getCounter(counters, 'סה"כ', 0);
+    const soldiersWoundedManeuver = getCounter(counters, 'סה"כ', 1);
+    return {
+      soldiersKilled,
+      soldiersKilledManeuver,
+      soldiersWounded,
+      soldiersWoundedManeuver,
+    };
+  } finally {
+    await browser?.close();
+  }
+}
--- bot/ironSwords/index.ts
+++ bot/ironSwords/index.ts
@@ -1,63 +1,12 @@
 import 'dotenv/config';
-import * as playwright from 'playwright-aws-lambda';
-import { JSDOM } from 'jsdom';
-import { Browser } from 'playwright-core';
+
 import NewWikiApi from '../wiki/NewWikiApi';
 import shabathProtectorDecorator from '../decorators/shabathProtector';
 import { getLocalDate } from '../utilities';
+import getCasualties from './idfSiteScraper';
 
 const baseTemplatePageName = 'תבנית:אבדות במלחמת חרבות ברזל';
 const templatePageName = `${baseTemplatePageName}/נתונים`;
-const url = 'https://www.idf.il/אתרי-יחידות/יומן-המלחמה/חללי-ופצועי-צה-ל-במלחמה/';
-
-function getCounter(
-  counters: Element[],
-  title: string,
-  index?: number,
-): number | null {
-  const elements = counters.filter((counter) => {
-    const text = counter.textContent;
-    return text?.includes(title);
-  });
-  if (elements.length > 1 && index == null) {
-    throw new Error(`Multiple counters with title ${title}`);
-  }
-  const element = elements[index ?? 0];
-  const numberElement = element?.querySelector('.counters');
-  const text = numberElement?.textContent;
-  if (!text) {
-    throw new Error(`Counter with title ${title} not found`);
-  }
-  return text ? Number(text.trim().replaceAll(',', '')) : null;
-}
-
-async function getCasualties() {
-  let browser: Browser | null = null;
-  try {
-    browser = await playwright.launchChromium({
-      headless: false,
-      timeout: 10 * 1000,
-    });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto(url);
-    const content = await page.content();
-    const { window } = new JSDOM(content);
-    const counters = Array.from(window.document.querySelectorAll('.counter-parent'));
-    const soldiersKilled = getCounter(counters, 'חללים מתחילת המלחמה');
-    const soldiersKilledManeuver = getCounter(counters, 'חללים מהתמרון בעזה');
-    const soldiersWounded = getCounter(counters, 'סה"כ', 0);
-    const soldiersWoundedManeuver = getCounter(counters, 'סה"כ', 1);
-    return {
-      soldiersKilled,
-      soldiersKilledManeuver,
-      soldiersWounded,
-      soldiersWoundedManeuver,
-    };
-  } finally {
-    await browser?.close();
-  }
-}
 
 const keysMapping = {
   חיילים: 'soldiersKilled',
