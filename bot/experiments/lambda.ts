a04216b63d27989d2de22740970787db63e5d455
Elad Heller <eladheller@gmail.com>
2025-02-18 20:45:40 +0200

    remove playwright.

--- bot/experiments/lambda.ts
+++ bot/experiments/lambda.ts
@@ -1,22 +1,5 @@
-import { chromium } from 'playwright';
-
+// eslint-disable-next-line no-empty-function
 export async function main() {
-  const browser = await chromium.launch({
-    headless: true,
-    timeout: 10 * 1000,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-gpu',
-      '--single-process',
-      '--no-zygote',
-    ],
-  });
-  const page = await browser.newPage();
-  await page.goto('https://www.example.com');
-  console.log(await page.title());
-  await browser.close();
 }
 
 export default main;
