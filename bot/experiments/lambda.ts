575d2322d2fd5df0621b08ca3222f445123623b1
Elad Heller <eladheller@gmail.com>
2025-02-18 20:31:35 +0200

    lambda: is docker redundant?

--- bot/experiments/lambda.ts
+++ bot/experiments/lambda.ts
@@ -1,7 +1,22 @@
-import ironSwordsBot from '../ironSwords';
+import { chromium } from 'playwright';
 
 export async function main() {
-  await ironSwordsBot();
+  const browser = await chromium.launch({
+    headless: true,
+    timeout: 10 * 1000,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+      '--single-process',
+      '--no-zygote',
+    ],
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.example.com');
+  console.log(await page.title());
+  await browser.close();
 }
 
 export default main;
