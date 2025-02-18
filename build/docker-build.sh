7332b76ebaebae2ff5056fa9f5b4cabb471f677f
Elad Heller <eladheller@gmail.com>
2025-02-18 03:21:20 +0200

    wiki bot playwright: fix for docker.

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region $AWS_REGION | \
 docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-npm run docker-build && \
-docker tag wiki-bot-playwright $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/wiki-bot-playwright:latest
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/wiki-bot-playwright:latest
+docker build -t wiki-bot-playwright:$IMAGE_VERSION -f ./build/wiki-bot-playwright/Dockerfile . && \
+docker tag wiki-bot-playwright:$IMAGE_VERSION $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/wiki-bot-playwright:$IMAGE_VERSION
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/wiki-bot-playwright:$IMAGE_VERSION
