d4f3e4342cc6fa81b3f94fb940f946e97f49976c
Elad Heller <eladheller@gmail.com>
2025-02-19 08:08:00 +0200

    docker: try to fix hard exit.

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region $AWS_REGION | \
 docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-docker build --platform=linux/amd64 -t wiki-bot-playwright:$IMAGE_VERSION -f ./build/wiki-bot-playwright/Dockerfile . && \
+docker build --platform=linux/amd64 -t --provenance=false wiki-bot-playwright:$IMAGE_VERSION -f ./build/wiki-bot-playwright/Dockerfile . && \
 docker tag wiki-bot-playwright:$IMAGE_VERSION $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/wiki-bot-playwright:$IMAGE_VERSION
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/wiki-bot-playwright:$IMAGE_VERSION
