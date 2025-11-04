1fa6e4f352e12d842bfe98baff965c3778378eab
Elad Heller <eladhe@wix.com>
2025-11-04 21:40:03 +0200

    BaseWikiApi: add user agent.

--- bot/experiments/index.ts
+++ bot/experiments/index.ts
@@ -1,7 +1,4 @@
-import europeanParliamentPartyUpdate from '../scripts/oneTime/europeanParliamentPartyUpdate';
-
 // eslint-disable-next-line no-empty-function
 async function main() {
-  await europeanParliamentPartyUpdate();
 }
 main();
