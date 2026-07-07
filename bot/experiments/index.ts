a23dab84d7adffa7ceb255a929efc6b76a34f1ce
Elad Heller <eladhe@wix.com>
2026-07-07 16:53:51 +0300

    experiments: there is no need to linter comment anymore.

--- bot/experiments/index.ts
+++ bot/experiments/index.ts
@@ -2,7 +2,6 @@ import WikiApi from '../wiki/WikiApi';
 
 process.env.NODE_ENV = 'development';
 
-// eslint-disable-next-line no-empty-function
 async function main() {
   const api = WikiApi();
   await api.login();
